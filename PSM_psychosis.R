f886a8748663b51caa82ccdeed5a6e12e4920d4b
Yasmin Gurleyen <160858802+yasmeme@users.noreply.github.com>
2024-10-15 19:31:30 +1100

    Update PSM_psychosis.R

--- PSM_psychosis.R
+++ PSM_psychosis.R
@@ -6,7 +6,7 @@ library(tidyr)
 library(dplyr)
 
 #read in excel file
-demog <- read_excel("/home/gucci/Documents/Imaging/Matching_samples_image_blood_pbmcs_July_2023.xlsx")
+demog <- read_excel("Matching_samples_image_blood_pbmcs_July_2023.xlsx")
 
 m.out0 <- matchit(Group ~ Sex + Ethnicity_Hispanic_or_Latino_or_of_Spanish_Origin + Race_Black_or_African_American + Race_Asian + Race_American_Indian_or_Alaskan_Native 
                   + Race_Native_Hawaiian_or_Other_Pacific_Islander + Race_White + Race_Unknown + Race_Other + Race_Multi + Age,
@@ -36,8 +36,8 @@ best_matched_group_0 <- group_0[1:40, ]
 best_matched_group_1 <- group_1[1:40, ]
 
 #print matched data
-write_xlsx(matched.data,"/home/gucci/Documents/Imaging/matched.data.xlsx")
+write_xlsx(matched.data,"~/matched.data.xlsx")
 
 #print 20 best matches for each group
-write_xlsx(best_matched_group_0,"/home/gucci/Documents/Imaging/best_matched_group_0.xlsx")
-write_xlsx(best_matched_group_1,"/home/gucci/Documents/Imaging/best_matched_group_1.xlsx")
+write_xlsx(best_matched_group_0,"~/best_matched_group_0.xlsx")
+write_xlsx(best_matched_group_1,"~/best_matched_group_1.xlsx")
